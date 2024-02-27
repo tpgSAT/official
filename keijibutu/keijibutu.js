932884a6784f5f120948b98c515e52f3daa5c5f3
ichi-sat <151018197+ichi-sat@users.noreply.github.com>
2024-02-28 00:50:58 +0900

    Prettier適用+TakiBar削除
    全ファイルPrettier適用しました＆ちょっとした文法ミス修正しました。
    
    TakiBarのディレクトリは削除しました。いらない子でした。

--- keijibutu/keijibutu.js
+++ keijibutu/keijibutu.js
@@ -1,26 +1,28 @@
-$(document).ready(function() {
-    $('.poster').click(function(){
-        $('.poster').css('padding-top','1.2rem');
-        $('.movie').css('padding-top','1rem');
-        $('.main1').css('display', 'block');
-        $('.main2').css('display', 'none');
-    });
-    $('.movie').click(function(){
-        $('.poster').css('padding-top','1rem');
-        $('.movie').css('padding-top','1.2rem');
-        $('.main1').css('display', 'none');
-        $('.main2').css('display', 'block');
-    });
-    $('.faq').each(function(){
-        var $q = $(this).find('.q');
-        var $a = $(this).find('.a');
+$(document).ready(function () {
+  $(".poster").click(function () {
+    $(".poster").css("padding-top", "1.2rem");
+    $(".movie").css("padding-top", "1rem");
+    $(".main1").css("display", "block");
+    $(".main2").css("display", "none");
+  });
+  $(".movie").click(function () {
+    $(".poster").css("padding-top", "1rem");
+    $(".movie").css("padding-top", "1.2rem");
+    $(".main1").css("display", "none");
+    $(".main2").css("display", "block");
+  });
+  $(".faq").each(function () {
+    var $q = $(this).find(".q");
+    var $a = $(this).find(".a");
 
-        $(this).find('.q').click(function(){
-            if ($a.is(':visible')) {
-                $a.slideUp();
-            } else {
-                $a.slideDown();
-            }
-        });
-    });
+    $(this)
+      .find(".q")
+      .click(function () {
+        if ($a.is(":visible")) {
+          $a.slideUp();
+        } else {
+          $a.slideDown();
+        }
+      });
+  });
 });
