bb5e0ed3d347b4df2285a6b4b2cd0fade186d535
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2026-01-26 23:57:35 +0900

    Add new sections and SVG assets for Blog, Events, News, and About pages
    - Created new SVG files for links to Blog, Events, and News sections.
    - Implemented AboutEn, BlogEn, EventsEn, and NewsEn components to display respective content.
    - Integrated new SVG assets into the components for visual links.
    - Styled components for responsive design across mobile and desktop views.

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,3 +1,5 @@
+const baseOrigin = import.meta.env.SITE; // Astroでビルド時に自動設定
+
 export const languages = {
     ja: '日本語',
     en: 'English',
@@ -11,4 +13,30 @@ export function useTranslations(lang: Lang) {
   return function t<T>(multilingual: Multilingual<T>): T {
     return multilingual[lang];
   };
+}
+
+type LocalePath = {
+  path: string;
+  lang: Lang;
+  label: (typeof languages)[Lang];
+};
+
+export function generateLocalePaths(url: URL): LocalePath[] {
+  const pathnames = url.pathname.split('/');
+
+  return Object.keys(languages).map((lang) => {
+    pathnames[1] = lang;
+    return {
+      path: pathnames.join('/').replace(/\/$/, ''),
+      lang: lang as Lang,
+      label: languages[lang as Lang],
+    };
+  });
+}
+
+export function generateLocaleUrls(url: URL): LocalePath[] {
+  return generateLocalePaths(url).map((localePath) => ({
+    ...localePath,
+    path: baseOrigin + localePath.path,
+  }));
 }
