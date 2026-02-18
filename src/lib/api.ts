a7cc957b6b748681b273dd63824f67c7b1050c26
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2026-02-19 01:48:06 +0900

    feat: GraphQLを型安全にするためにcodegenを導入

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,111 +1,16 @@
-export async function getPosts() {
-  const allPosts = [];
-  let page = 1;
-  let totalPages = 1;
+import { GraphQLClient } from "graphql-request";
 
-  do {
-    const res = await fetch(`https://cms.tpgd.jp/wp-json/wp/v2/posts?per_page=100&page=${page}&_embed`);
-    if (!res.ok) break;
+export const wpClient = new GraphQLClient("https://cms.tpgd.jp/graphql", {
+  headers: {
+    // 認証が必要ならここに token など
+  },
+});
 
-    const posts = await res.json();
-    totalPages = parseInt(res.headers.get('X-WP-TotalPages')) || 1;
-    allPosts.push(...posts);
-    page++;
-  } while (page <= totalPages);
+import { getSdk } from "./generated/graphql";
 
-  return allPosts;
-}
+const sdk = getSdk(wpClient);
 
-interface gqlParams {
-    query: String;
-    variables?: object;
-}
-
-export async function wpquery({ query, variables = {} }: gqlParams) {
-    const res = await fetch('https://cms.tpgd.jp/graphql', {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json",
-
-        },
-
-        body: JSON.stringify({
-            query,
-            variables,
-        }),
-    });
-
-    if (!res.ok) {
-        console.error(res);
-        return {};
-    }
-
-    const { data } = await res.json();
-    return data;
-}
-
-export async function getAllPages() {
-  const data = await wpquery({
-  query: `
-  {
-    posts(first: 10000) {
-      edges {
-        node {
-          slug
-          excerpt
-				  title
-          date
-          categories {
-            edges {
-              node {
-                name
-              }
-            }
-          }
-          tags {
-            edges {
-              node {
-                name
-              }
-            }
-          }
-          featuredImage {
-            node {
-              sourceUrl
-              altText
-              mediaDetails {
-                sizes {
-                  name
-                  sourceUrl
-                  width
-                  height
-                }
-              }
-            }
-          }
-          content
-        }
-      }
-    }
-  }
-  `});
-  console.log(data);
-  return data.posts.edges;
-}
-
-export async function getPostBySlug(slug) {
-  const data = await wpquery({
-    query: `
-      query GetPostBySlug($slug: ID!) {
-        post(id: $slug, idType: SLUG) {
-          id
-          title
-          uri
-        }
-      }
-    `,
-    variables: { slug },
-  });
-
-  return data.post;
+export async function getAllPosts() {
+  const res = await sdk.GetAllPosts();
+  return res.posts?.edges;
 }
