29a9c055d5e2f6e0cb41f1c9ec71421ee13a3cb8
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2026-02-19 00:05:22 +0900

    feat: wordpressからのフェッチをrest apiからgraphqlに変更

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -15,3 +15,97 @@ export async function getPosts() {
 
   return allPosts;
 }
+
+interface gqlParams {
+    query: String;
+    variables?: object;
+}
+
+export async function wpquery({ query, variables = {} }: gqlParams) {
+    const res = await fetch('https://cms.tpgd.jp/graphql', {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+
+        },
+
+        body: JSON.stringify({
+            query,
+            variables,
+        }),
+    });
+
+    if (!res.ok) {
+        console.error(res);
+        return {};
+    }
+
+    const { data } = await res.json();
+    return data;
+}
+
+export async function getAllPages() {
+  const data = await wpquery({
+  query: `
+  {
+    posts(first: 10000) {
+      edges {
+        node {
+          slug
+          excerpt
+				  title
+          date
+          categories {
+            edges {
+              node {
+                name
+              }
+            }
+          }
+          tags {
+            edges {
+              node {
+                name
+              }
+            }
+          }
+          featuredImage {
+            node {
+              sourceUrl
+              altText
+              mediaDetails {
+                sizes {
+                  name
+                  sourceUrl
+                  width
+                  height
+                }
+              }
+            }
+          }
+          content
+        }
+      }
+    }
+  }
+  `});
+  console.log(data);
+  return data.posts.edges;
+}
+
+export async function getPostBySlug(slug) {
+  const data = await wpquery({
+    query: `
+      query GetPostBySlug($slug: ID!) {
+        post(id: $slug, idType: SLUG) {
+          id
+          title
+          uri
+        }
+      }
+    `,
+    variables: { slug },
+  });
+
+  return data.post;
+}
